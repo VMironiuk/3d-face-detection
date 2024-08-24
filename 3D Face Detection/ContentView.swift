4fa526c3d8ac99919e912763209b2dcd9b5a5ff8
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-08-24 11:07:52 +0300

    Add starter project

---
+++ 3D Face Detection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3D Face Detection
+//
+//  Created by Volodymyr Myroniuk on 24.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ 3D Face Detection/FaceDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectionApp.swift
+//  3D Face Detection
+//
+//  Created by Volodymyr Myroniuk on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
